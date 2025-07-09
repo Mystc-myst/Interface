b72b7743103db2b066e10d33682d14bc84cd3e5c
Mystc-myst <jochen.bohr@gmail.com>
2025-07-09 12:20:47 +0200

    Update HashtagButtons.js

--- lune-interface/client/src/components/HashtagButtons.js
+++ lune-interface/client/src/components/HashtagButtons.js
@@ -13,7 +13,7 @@ function HashtagButtons({ hashtags, onHashtagClick }) {
           key={tag}
           type="button" // Important to prevent form submission if inside a form
           onClick={() => onHashtagClick(tag)}
-          className="px-4 py-2 bg-luneSecondary text-luneText font-semibold rounded-full text-base hover:bg-lunePurple hover:text-white focus:outline-none focus:ring-2 focus:ring-lunePurple-dark focus:ring-opacity-75 transition-colors duration-150 shadow-sm"
+          className="btn-liquid-gold-translucent" // Applied the new class here
         >
           {tag}
         </button>
@@ -28,3 +28,4 @@ HashtagButtons.propTypes = {
 };
 
 export default HashtagButtons;
+
